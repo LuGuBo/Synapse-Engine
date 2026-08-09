6acfd6c6c9384eae83d47be8fb05723bba4bd81c
LuGuBo <lgbonanni@gmail.com>
2026-08-09 19:11:04 -0300

    fix(ci): add explicit jest.config.js to restrict testMatch to .test.js and bump v2.2.5

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: [
+    '<rootDir>/tests/**/*.test.js'
+  ],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '/tests/temp_test_project/',
+    '/tests/temp_'
+  ],
+  verbose: true
+};
